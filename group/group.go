9716fbedee2acb1444a720f4e5182d6402362b7f
Parham Alvani <parham.alvani@gmail.com>
2018-02-23 11:14:56 +0330

    Expose group fields.

--- group/group.go
+++ group/group.go
@@ -6,20 +6,20 @@ type Sensor string
 // Group represents group of sensors under
 // single point management
 type Group struct {
-	id      string
-	sensors []Sensor
+	ID      string
+	Sensors []Sensor
 }
 
 // New creates new group with given identification
 // and empty array of sensors.
 func New(id string) *Group {
 	return &Group{
-		id:      id,
-		sensors: make([]Sensor, 0),
+		ID:      id,
+		Sensors: make([]Sensor, 0),
 	}
 }
 
 // Add adds new sensor into group
 func (g *Group) Add(s Sensor) {
-	g.sensors = append(g.sensors, s)
+	g.Sensors = append(g.Sensors, s)
 }
